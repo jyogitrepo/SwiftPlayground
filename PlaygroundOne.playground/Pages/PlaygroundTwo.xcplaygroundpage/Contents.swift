b2de57eb3745ab64f69b70957dacee7e93c0bd96
Joe <jyohere.d@gmail.com>
2019-11-03 15:58:08 +0900

    Commit two playground

--- PlaygroundOne.playground/Pages/PlaygroundTwo.xcplaygroundpage/Contents.swift
+++ PlaygroundOne.playground/Pages/PlaygroundTwo.xcplaygroundpage/Contents.swift
@@ -1,7 +1,41 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+var age = 18
+let dob = "20/11/1920"
 
-//: [Next](@next)
+var integer: Int = 18
+var double: Double = 20.50
+var character: Character = "C"
+var boolean: Bool = false
+var string: String = "String"
+string.count
+
+var optionalInt: Int?
+optionalInt = 100
+
+//print(optionalInt)
+print(optionalInt!)
+print(optionalInt ?? "nil")
+
+if let tempInt = optionalInt {
+    print(tempInt) // tempInt only accessible inside
+}
+
+if optionalInt != nil {
+    print(optionalInt)
+}
+
+func testGuard() {
+    guard let tempInt = optionalInt else {
+        print("optionalInt is nil")
+        return
+    }
+    print("Value is \(tempInt)")
+}
+
+testGuard()
+optionalInt = nil
+testGuard()
+
+
+
