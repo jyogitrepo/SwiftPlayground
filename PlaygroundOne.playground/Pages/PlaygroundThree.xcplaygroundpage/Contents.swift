833833ee32ecc00dec9835c19fc87cf4d0a1c132
Joe <jyohere.d@gmail.com>
2019-11-03 15:58:42 +0900

    First Commit

--- PlaygroundOne.playground/Pages/PlaygroundThree.xcplaygroundpage/Contents.swift
+++ PlaygroundOne.playground/Pages/PlaygroundThree.xcplaygroundpage/Contents.swift
@@ -1,7 +1,41 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+var optionalInt: Int?
+var optionalString: String?
 
-//: [Next](@next)
+func testGuard() {
+    guard let tempInt = optionalInt,
+           let tempString = optionalString else {
+            print("tempInt/tempString is nil")
+            return // always needed in guard or throw
+    }
+    print(tempInt)
+    print(tempString)
+}
+
+testGuard()
+
+optionalInt = 10
+
+testGuard()
+
+optionalString = "string"
+
+testGuard()
+
+print()
+
+// Equivalent to testGuard()
+func testAnotherGuard() {
+    guard optionalInt != nil,
+           optionalString != nil else {
+            print("testAnotherGuard: tempInt/tempString is nil")
+            return // always needed in guard or throw
+    }
+    print("testAnotherGuard \(optionalInt!)")
+    print("testAnotherGuard \(optionalString!)")
+}
+
+testAnotherGuard()
+optionalInt = nil
+testAnotherGuard()
